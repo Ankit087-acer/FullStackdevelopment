0e45ef79c52401cd359122e914d1feac22a2259c
Ankit Parida <ankitparida386@gmail.com>
2025-08-16 12:00:19 +0530

    Adding the new changes

---
+++ JAVASCRIPT/Conditional/index.js
@@ -0,0 +1,48 @@
+console.log("Hello,I am conditional tutorial")
+//if else ladder
+// let age=6;
+// if(age>=6){
+//     console.log("Greater than 5");
+// }
+// else{
+//     console.log("No");
+// }
+
+//increment decrement operator
+// let a=67;
+// console.log(a++);
+// console.log(a);
+// console.log(++a);
+// console.log(a);
+// console.log(a--);
+// console.group(a);
+// console.log(--a);
+
+//logical operators
+// let a=65;
+// let b=45;
+// let c=89
+// if(a>65 || b>34){    //&&
+//     console.log("True");
+// }
+// else {
+//     console.log("True")
+// }
+// console.log(!(c>2));
+
+// if if-else else-if
+// let age=76;
+// if(age>=18){
+//     console.log("You can drive")
+// }
+// else if(age==0){
+//     console.log("Wrong")
+// }
+// else{
+//     console.log("You cannot drive")
+// }
+
+//Ternary opertor
+// let age=67;
+// let c=(age>18)?(("You can drive")):(("You cannot drive"))
+// console.log(c)
---
+++ NODEJS/ModulesJs/math.js
@@ -0,0 +1,3 @@
+const math=require("./connect")
+console.log(math.add(2,5))
+console.log(math.sub(5,6))
