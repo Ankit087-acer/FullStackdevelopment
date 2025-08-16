78aa76367bb2aabcef1e69244b318f4fa13b7707
Ankit Parida <ankitparida386@gmail.com>
2025-08-16 13:07:01 +0530

    Adding the Changes

--- NODEJS/ModulesJs/connect.js
+++ NODEJS/ModulesJs/connect.js
@@ -1,14 +1,20 @@
-function add(a,b){
-    return a+b;
-}
-function sub(a,b){
-    return a-b;
-}
-module.exports={
-    add, sub
-};
+// function add(a,b){
+//     return a+b;
+// }
+// function sub(a,b){
+//     return a-b;
+// }
+// module.exports={
+//     add, sub
+// };
 //2nd option
 // module.exports={
 //     addfn:add,
 //     subfn:sub,
 // }
+
+//3rd option
+module.exports={
+    add:(a,b)=>a + b,
+    sub:(a,b)=>a-b
+};
--- NODEJS/ModulesJs/math.js
+++ NODEJS/ModulesJs/math.js
@@ -1,3 +1,5 @@
 const math=require("./connect")
-console.log(math.add(2,5))
-console.log(math.sub(5,6))
+// console.log(math.add(2,5))
+// console.log(math.sub(5,6))
+console.log(math.add(4,5));
+console.log(math.sub(3,4));
